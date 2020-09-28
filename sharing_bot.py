977766cf6bf718dfcd9f599cc0c671fbf575255a
skjerns <14980558+skjerns@users.noreply.github.com>
2020-09-28 18:41:12 +0200

    Update sharing_bot.py

--- sharing_bot.py
+++ sharing_bot.py
@@ -2,37 +2,110 @@
 """
 Created on Sat Sep 26 13:10:53 2020
 
+TODO: argparse input stuff
+
 @author: skjerns
 """
 import os
 from telepot.loop import MessageLoop
 import telepot
-from pprint import pprint as print, pformat
 import time
+from pprint import pprint as pprint, pformat
 import json
-import shelve
-from sqlitedict import SqliteDict
+import urllib
 
-token = 'xxxxxxx'
+try:
+    print('Testing connection')
+    urllib.request.urlopen('http://api.telegram.org/')
+    print('Success.')
+except:
+    try:
+        print('Trying again with proxy')
+        telepot.api.set_proxy('http://proxy.server:3128')
+        print('Success with proxy.')
+    except:
+        raise
 
-bot = telepot.Bot(token)
+token = ''
+
+settings_file = os.path.expanduser('~/.sharingiskaringbot')
 
 
-def get_user(user_id):
-    
-        
-def handle(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    print(msg)
-    # if content_type=='new_chat_member':
-        
-    bot.sendMessage(chat_id, f'type: {content_type}')
-    bot.sendMessage(chat_id, f'chat: {chat_type}')
-    bot.sendMessage(chat_id, f'```\n{pformat(msg)}\n```', 
-                    parse_mode='MarkdownV2')
-    time.sleep(10)
-    with SqliteDict('./my_db.json', encode=json.dumps, decode=json.loads) as db:
-        db['test']    = 'test'     
+with open(settings_file) as f:
+    settings = json.load(f)
+    token = settings['token']
+    admin_chat_id = settings['admin_chat_id']
+    group_chat_id = settings['group_chat_id']
 
-    
-MessageLoop(bot, handle).run_as_thread()
+
+class Member():
+    def __init__(self, member):
+        self.id = member.get('id')
+        self.first = member.get('first_name', '')
+        self.last = member.get('last_name', '')
+        self.is_bot = member.get('is_bot', False)
+
+    def send_message(self, msg, bot):
+        pass
+
+    def __eq__(self, other):
+        return self.id==other.id
+
+
+class Bot(telepot.Bot):
+    def forward_new_user_messages(self, msg):
+        from_member = Member(msg['from'])
+        for new_member in map(Member, msg['new_chat_members']):
+            if from_member==new_member:
+                message = f'[{new_member.first} {new_member.last}](tg://user?id={new_member.id}) joined'
+            else:
+                message = f'[{from_member.first} {from_member.last}](tg://user?id={from_member.id}) invited '\
+                          f'[{new_member.first} {new_member.last}](tg://user?id={new_member.id})'
+            bot.sendMessage(admin_chat_id, message, disable_notification=True,
+                            parse_mode = 'MarkdownV2')
+
+    def forward_user_left_messages(self, msg):
+        from_member = Member(msg['from'])
+        old_member = Member(msg['left_chat_member'])
+        if from_member==old_member:
+            message = f'[{old_member.first} {old_member.last}](tg://user?id={old_member.id}) left'
+        else:
+            message = f'[{from_member.first} {from_member.last}](tg://user?id={from_member.id}) removed '\
+                      f'[{old_member.first} {old_member.last}](tg://user?id={old_member.id})'
+        bot.sendMessage(admin_chat_id, message, disable_notification=True,
+                        parse_mode = 'MarkdownV2')
+
+
+    def log(self):
+        pass
+
+    def hdl(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        msg_id = telepot.message_identifier(msg)
+
+        try:
+            if content_type=='new_chat_member':
+                self.deleteMessage(msg_id)
+                self.forward_new_user_messages(msg)
+            elif content_type=='left_chat_member':
+                self.deleteMessage(msg_id)
+                self.forward_user_left_messages(msg)
+            else:
+                self.sendMessage(admin_chat_id, f'type: {content_type}\nchat: {chat_type}\n```\n{pformat(msg)}\n```',
+                            parse_mode='MarkdownV2', disable_notification=True)
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            pprint(f'ERROR, wait 2 sec :{pformat(str(e)), pformat(repr(e))}')
+            time.sleep(2)
+        pprint('-'*10 + '\n' + pformat(msg))
+
+
+bot = Bot(token)
+MessageLoop(bot, bot.hdl).run_as_thread()
+
+print("Bot running...")
+
+while True:
+    time.sleep(5)
+    print('.', end='', flush=True)
