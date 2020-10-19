c48db4a7e63b4e114999eaaa7d26ec9538f92672
skjerns <14980558+skjerns@users.noreply.github.com>
2020-10-19 10:37:50 +0200

    test

--- sharing_bot.py
+++ sharing_bot.py
@@ -7,12 +7,15 @@ TODO: argparse input stuff
 @author: skjerns
 """
 import os
-from telepot.loop import MessageLoop
 import telepot
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+from telepot.namedtuple import KeyboardButton, ReplyKeyboardMarkup, ForceReply
 import time
 from pprint import pprint as pprint, pformat
 import json
 import urllib
+import datetime
+import traceback
 
 try:
     print('Testing connection')
@@ -26,10 +29,8 @@ except:
     except:
         raise
 
-token = ''
 
 settings_file = os.path.expanduser('~/.sharingiskaringbot')
-
 
 with open(settings_file) as f:
     settings = json.load(f)
@@ -45,6 +46,7 @@ class Member():
         self.first = member.get('first_name', '')
         self.last = member.get('last_name', '')
         self.is_bot = member.get('is_bot', False)
+        self.username = member.get('username', '')
 
     def send_message(self, msg, bot):
         pass
@@ -54,14 +56,48 @@ class Member():
 
 
 class Bot(telepot.Bot):
+
+    def __init__(self, token):
+        super(Bot, self).__init__(token)
+        self.offset = None
+
+    def run(self):
+        print('Bot running.')
+        while True:
+            try:
+
+                updates = self.getUpdates(self.offset)
+                for update in updates:
+                    self.offset = update['update_id'] + 1
+                    message = update['message']
+                    self.process(message)
+                print('.' if len(updates)==0 else '!'*len(updates), end='')
+
+            except KeyboardInterrupt:
+                raise
+
+            except telepot.exception.BadHTTPResponse as e:
+                traceback.print_exc()
+
+                # Servers probably down. Wait longer.
+                if e.status == 502:
+                    print('Server down? wait 30 seconds')
+                    time.sleep(30)
+
+            except:
+                traceback.print_exc()
+
+            finally:
+                time.sleep(0.3)
+
     def forward_new_user_messages(self, msg):
         from_member = Member(msg['from'])
         chat_name = msg['chat'].get('username')
         for new_member in map(Member, msg['new_chat_members']):
             if from_member==new_member:
-                message = f'<a href="tg://user?id={new_member.id}">{new_member.first} {new_member.last}</a> joined {chat_name}'
+                message = f'<a href="tg://user?id={new_member.id}">{new_member.first} {new_member.last} ({new_member.username})</a> joined {chat_name}'
             else:
-                message = f'<a href="tg://user?id={from_member.id}">{from_member.first} {from_member.last}</a> added '\
+                message = f'<a href="tg://user?id={from_member.id}">{from_member.first} {from_member.last} ({new_member.username})</a> added '\
                           f'<a href="tg://user?id={new_member.id}">{new_member.first} {new_member.last}</a> to {chat_name}'
             bot.send_message(admin_chat_id, message, disable_notification=True,
                             parse_mode = 'html')
@@ -78,12 +114,31 @@ class Bot(telepot.Bot):
         bot.send_message(admin_chat_id, message, disable_notification=True,
                         parse_mode = 'html')
 
+    def send_not_answer_reminder(self, msg):
+        from_member = Member(msg['from'])
+        message_id = msg['message_id']
+        chat_id = msg['chat']['id']
+        text = 'Du hast gerade in der SharingIsKAring-Gruppe auf eine Nachricht geantwortet. ' \
+               'Bitte benutze die Antworten-Funktion nur in Ausnahmefällen und schreibe sonst alles mit der Person in einem <b>privaten Chat</b>\n' \
+               'Dies gilt insbesondere für <i>"Ich habe Interesse", "Danke"</i>, etc.\n\n' \
+               'Bitte lösche deine Nachricht wieder und sende sie als <b>private Nachricht</b> (außer du denkst, sie ist wirklich für alle 500+ Leute relevant.). Beep-boop, ich bin ein Bot🤖.'
 
-    def log(self):
-        pass
+        keyboard = ReplyKeyboardMarkup(keyboard=[
+                   [InlineKeyboardButton(text='Ja', switch_inline_query=''),
+                    InlineKeyboardButton(text='Nein', switch_inline_query_current_chat='@SharingIsKAringBot no-inline')],
+                   ], selective=True, one_time_keyboard=True, resize_keyboard=False)
+
+        self.send_message(chat_id, '/deleteme', parse_mode='html', reply_markup=ForceReply(),
+                          reply_to_message_id=message_id, disable_notification=True)
+
+
+    def log(self, msg):
+        now = datetime.datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')
+        with open('./bot.log', 'w') as f:
+            f.write(f'{now} {msg}\n')
 
     def send_message(self, chat_id,  message, disable_notification=False,
-                        parse_mode='MarkdownV2'):
+                        parse_mode='html', **kwargs):
         if parse_mode.lower()=='markdownv2':
             reserved = "#'_*[]()~`#+-|{}.!>"
         else:
@@ -91,9 +146,9 @@ class Bot(telepot.Bot):
         for char in reserved:
             message = message.replace(char, f'\\{char}')
         self.sendMessage(chat_id, message, disable_notification=disable_notification,
-                         parse_mode=parse_mode)
+                         parse_mode=parse_mode, **kwargs)
 
-    def hdl(self, msg):
+    def process(self, msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
         msg_id = telepot.message_identifier(msg)
 
@@ -103,17 +158,30 @@ class Bot(telepot.Bot):
                             parse_mode='MarkdownV2', disable_notification=True)
                 self.send_message(chat_id, f'Hi, I\'m a bot. Please see my source code at https://github.com/skjerns/SharingIsKAringBot.\n'\
                                            f'You sent me "{msg["text"]}", but I have no idea what that means.', parse_mode='MarkdownV2')
-            elif chat_type=='supergroup' and (('text' in msg) or ('photo' in msg)):
-                pass
-            elif content_type=='new_chat_member':
-                self.deleteMessage(msg_id)
-                self.forward_new_user_messages(msg)
-            elif content_type=='left_chat_member':
-                self.deleteMessage(msg_id)
-                self.forward_user_left_messages(msg)
+            elif chat_type=='supergroup' and  str(chat_id)==str(group_chat_id):
+                print(f'its a group message: {content_type}')
+
+                if content_type=='new_chat_member':
+                    # remove new chat member message and forward to admin chat
+                    self.deleteMessage(msg_id)
+                    self.forward_new_user_messages(msg)
+
+                elif content_type=='left_chat_member':
+                    # remove chat member deleted message and forward to admin chat
+                    self.deleteMessage(msg_id)
+                    self.forward_user_left_messages(msg)
+
+                elif content_type=='text' and ('reply_to_message' in msg) and \
+                     not msg['reply_to_message']['from']['is_bot']:
+                    # remind users not to answer in group
+                    print('reminding')
+                    self.send_not_answer_reminder(msg)
+
             else:
-                self.send_message(debug_chat_id, f'type: {content_type}\nchat: {chat_type}\n```\n{pformat(msg)}\n```',
-                            parse_mode='MarkdownV2', disable_notification=True)
+                # if none of the above: send debug message.
+                self.send_message(debug_chat_id,
+                            f'No action taken.\ntype: {content_type}\nchat: {chat_type}\n```\n{pformat(msg)}\n```',
+                            parse_mode='html', disable_notification=True)
         except Exception as e:
             import traceback
             traceback.print_exc()
@@ -123,16 +191,7 @@ class Bot(telepot.Bot):
         print('-'*10)
         pprint(msg)
 
-
 bot = Bot(token)
-MessageLoop(bot, bot.hdl).run_as_thread()
+bot.run()
 
-print("Bot running...")
 
-while True:
-    try:
-        time.sleep(5)
-        print('.', end='', flush=True)
-    except Exception as e:
-        bot.send_message(debug_chat_id, f'Script ended: {time.ctime()}')
-        bot.send_message(debug_chat_id, f'{str(e)} {repr(e)}')
